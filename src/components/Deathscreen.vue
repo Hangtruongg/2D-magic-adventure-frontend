5a94bdba08f67cb44c864b9f8f8d57f882405312
Thi Thanh Hang Truong <thanhhang2x2@gmail.com>
2024-06-30 21:00:46 +0200

    update Deathscreen and Winscreen

--- src/components/Deathscreen.vue
+++ src/components/Deathscreen.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="container">
+    <div id="deathscreen">
         <div id="buttons">
-            <h1>You Died!</h1>
+            <h1>You lose!</h1>
             <button class = "deathScreenButton" @click="navigateToGame">Restart level</button>
             <button class = "deathScreenButton" @click="navigateToHome">Return to home page</button>
         </div>
@@ -38,5 +38,34 @@ padding: 15px 30px;
 text-align: center;
 margin: 0 200px;
 cursor: pointer;
+}
+
+h1 {
+  font-size: 60px;
+  animation: changeColor 5s infinite;
+  margin-top: 250px;
+  margin-left: 480px;
+}
+
+
+@keyframes changeColor {
+  0% {
+    color: violet; 
+  }
+  10% {
+    opacity: 1;
+  }
+  25% {
+    color: rgb(191, 39, 130); 
+  }
+  50% {
+    color: rgb(66, 5, 45); 
+  }
+  75% {
+    color: rgb(213, 162, 197); 
+  }
+  100% {
+    color: violet; 
+  }
 }
 </style>
