0651e691f65e5a34f07cf60c1e6cffdb47a737dc
Massimo Marsiglia <marsigliamassi@gmail.com>
2024-07-01 00:01:51 +0200

    DEPLOY TEST

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'json', 'vue'],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.js$': 'babel-jest'
-  },
-  testMatch: ['/tests//*.spec.js'],
-  setupFiles: ['./jest.setup.js']
-};
+// module.exports = {
+//   moduleFileExtensions: ['js', 'json', 'vue'],
+//   transform: {
+//     '^.+\\.vue$': 'vue-jest',
+//     '^.+\\.js$': 'babel-jest'
+//   },
+//   testMatch: ['/tests//*.spec.js'],
+//   setupFiles: ['./jest.setup.js']
+// };
